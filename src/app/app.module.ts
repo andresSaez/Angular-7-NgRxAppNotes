ef8ed613869db2d6de1a2e176d7960a0b0fac481
Andres Saez <andsc0830900@gmail.com>
2019-03-18 14:34:27 +0100

    finishing section5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,12 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 // ngrx
 import { StoreModule } from '@ngrx/store';
-import { todoReducer } from './todo/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment'; // Angular CLI environemnt
+import { appReducers } from './app.reducers';
+
+// Forms
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,11 +30,12 @@ import { environment } from '../environments/environment'; // Angular CLI enviro
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ todos: todoReducer }),
+    ReactiveFormsModule,
+    StoreModule.forRoot( appReducers ),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
-    }),
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { ToggleTodoAction, EditarTodoAction, BorrarTodoAction } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() todo: Todo;
+  @ViewChild('txtInputFisico') txtInputFisico: ElementRef;
+
+  checkField: FormControl;
+  txtInput: FormControl;
+
+  editando: boolean;
+
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+    this.checkField = new FormControl( this.todo.completado );
+    this.txtInput = new FormControl( this.todo.texto, Validators.required );
+
+    this.checkField.valueChanges
+      .subscribe( () => {
+        const action = new ToggleTodoAction( this.todo.id );
+        this.store.dispatch( action );
+      });
+  }
+
+  editar() {
+    this.editando = true;
+
+    setTimeout( () => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+
+    if ( this.txtInput.invalid ) {
+      return;
+    }
+
+    if ( this.txtInput.value === this.todo.texto ) {
+      return;
+    }
+
+    const action = new EditarTodoAction( this.todo.id, this.txtInput.value );
+    this.store.dispatch( action );
+  }
+
+  borrarTodo() {
+    const action = new BorrarTodoAction( this.todo.id );
+    this.store.dispatch( action );
   }
 
 }
